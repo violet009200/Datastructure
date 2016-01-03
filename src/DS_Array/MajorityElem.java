1b85498a95c7549453ba02377bacb8eb902b0b31
Bora Park <violet009200@gmail.com>
2016-01-02 19:33:51 -0600

    majorityElem

---
+++ src/DS_Array/MajorityElem.java
@@ -0,0 +1,11 @@
+package DS_Array;
+
+//Majority Element: A majority element in an array A[] of size n 
+//is an element that appears more than n/2 times (and hence there is at most one such element).
+
+public class MajorityElem {
+	
+	public static void main(String[] args){
+		
+	}
+}
