328d29ce7d3f902da2168203dc64b6770b4c3c07
Bora Park <violet009200@gmail.com>
2016-01-19 13:41:06 -0600

    Stair

---
+++ src/DrawbyStar.java
@@ -0,0 +1,19 @@
+
+//draw a rectangle by stars and numbers as shown   below 1 12 123 1234 12345 * ** *** **** ***** ****** 
+
+public class DrawbyStar {
+	
+	
+	public static void main(String[] args){
+	for(int i=1;i<5; i++){
+		for(int j=1; j<5; j++){
+			if(i==j)
+				for(int k = 0;k<j; k++)
+					System.out.print("*");
+			//else
+				//System.out.print(" ");
+		}
+		System.out.println("");
+	}
+	}
+}
