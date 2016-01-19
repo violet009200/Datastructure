42f2d9c9fe88603d1bfa7199c68f9c3eccb673ca
Bora Park <violet009200@gmail.com>
2016-01-19 13:03:33 -0600

    find Common Numbers in Three arrays

---
+++ src/DS_Array/CommonNumberInArrays.java
@@ -0,0 +1,37 @@
+package DS_Array;
+
+import java.util.Arrays;
+
+/*
+ You have three very big sorted arrays; print out the common number if any other wise print no such a number.
+ */
+
+
+public class CommonNumberInArrays {
+	
+	public static void main(String[] args){
+		int[] arr1 = {1, 5, 10, 20, 40, 80};
+		int[] arr2 = {6, 7, 20, 80, 100};
+		int[] arr3 = {3,4,15,20,30,70,80,120};
+		
+		
+		int i= 0;
+		int j= 0;
+		int k = 0;
+		
+		while(i<arr1.length && j<arr2.length && k<arr3.length){
+			if(arr1[i] == arr2[j] && arr2[j] == arr3[k]){
+				System.out.print(arr1[i]+" ");
+				i++;
+				j++;
+				k++;
+			}
+			else if(arr1[i]<arr2[j]) i++;
+			else if(arr2[j]<arr3[k]) j++;
+			else k++;
+			
+		}
+		
+		
+	}
+}
