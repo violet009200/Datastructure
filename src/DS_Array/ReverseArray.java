69347942e30d0a5e71a301b1f81bd9af78bde3e7
Bora Park <violet009200@gmail.com>
2016-02-03 19:12:17 -0600

    geeksforgeek

--- src/DS_Array/ReverseArray.java
+++ src/DS_Array/ReverseArray.java
@@ -1,22 +1,56 @@
 package DS_Array;
 
 import java.util.Arrays;
+import java.util.Scanner;
+/*
+Input
+1		//test case
+4		//number of elem in array
+1 2 3 4	//array
+
+Output:
+4 3 2 1
+ */
 
 public class ReverseArray {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int[] arr = {1,2,3,4,5,6,7,8,9,10};
-		int j = arr.length-1;
-		for(int i = 0; i<arr.length/2; i++){
-			int tmp = arr[i];
-			arr[i] = arr[j];
-			arr[j] = tmp;
+	
+		public static void main (String[] args) {
+			Scanner sc = new Scanner(System.in);
+			int T = sc.nextInt();
 			
-			j--;
+			//System.out.println(T);
+			for(int i=0;i<T;i++){
+			    int N = sc.nextInt();
+			   // System.out.println(N);
+			    int[] arr = new int[N];
+			    for(int j=0; j<N;j++){
+			        arr[j] = sc.nextInt();
+			    }
+			   //System.out.println(Arrays.toString(arr));
+			    reverseArr(arr);
+			}
 		}
 		
-		System.out.println(Arrays.toString(arr));
-	}
+		public static void reverseArr(int[] arr){
+		
+		    int i = arr.length-1;
+		    
+		    for(int j=0; j<arr.length/2; j++){
+		    	
+		        int tmp = arr[j];
+		        arr[j]= arr[i];
+		        arr[i] = tmp;
+		        i--;
+		    }
+		    
+		    
+		    //print array
+		    for(int j=0; j<arr.length;j++){
+		        System.out.print(arr[j]+" ");
+		    }
+		    System.out.println("");
+		}
 
+	
 }
